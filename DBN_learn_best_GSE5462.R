7940bafc16ada0b9009743340ebb97cd0b05d68a
pminkina <polina.suter@bsse.ethz.ch>
2022-06-20 00:29:14 +0200

    code fix

--- DBN_learn_best_GSE5462.R
+++ DBN_learn_best_GSE5462.R
@@ -9,8 +9,12 @@
 #Initial and transition structures are learned separately, i.e. samestruct = FALSE
 
 
-#data for the dataset GSE5462: differentially expressed genes and their transcription factors
-dbndata<-readRDS("data/data_125.rds")
+#data for the dataset GSE5462
+#original data can be found here
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE5462
+#selected features (dbndata) include differentially expressed genes and their transcription factors
+#dbndata<-readRDS("data/data_125.rds")
+
 #STRING based blacklisting
 curbl<-readRDS("data/bl_125.rds")
 #threshold for consensus model
@@ -23,11 +27,15 @@ set.seed(100)
 scoreall<-scoreparameters("bge", dbndata, dbnpar = list(samestruct = FALSE, slices = 2, b = 0),
                           DBN = TRUE, edgepmat = NULL)
 #find MAP dag
-fitall <- iterativeMCMC(scoreall, verbose = FALSE, blacklist=curbl,hardlimit=9,plus1it=7)
+fitall <- iterativeMCMC(scoreall, verbose = TRUE, blacklist=curbl,hardlimit=9,plus1it=7)
 #obtain a sample from the posterior distribution
-sampall <- orderMCMC(scoreall, verbose = FALSE, startspace=fitall$endspace,
+sampall <- orderMCMC(scoreall, verbose = FALSE, startspace=fitall$endspace, MAP=FALSE,
                      chainout = TRUE, blacklist=curbl)
 #estimate consensus model by performing model averaging
 consall<-modelp(sampall,p=p,pdag=FALSE)
+
+compareDBNs(consall,getDAG(fitall))
+#MAP model performed better for classification hence it was used for visualization
+
 
 
