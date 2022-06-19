f516656db7c1c37fc4983c45000aae6242966d53
pminkina <polina.suter@bsse.ethz.ch>
2022-06-20 01:36:38 +0200

    code fix

--- DBN_learn_best_GSE37182.R
+++ DBN_learn_best_GSE37182.R
@@ -9,20 +9,30 @@
 
 
 #search space for sampling identified using iterativeMCMC for both groups
-commonspace<-readRDS("data/comcolon122.rds")
+commonspace<-readRDS("/Users/polinasuter/Downloads/DBNs/DBNclass/data/comcolon122.rds")
 #data for the dataset data for the dataset GSE37182: differentially expressed genes and their transcription factors
-datanorm<-readRDS("data/datanorm122.rds")
-datacancer<-readRDS("data/datacancer122.rds")
+datanorm<-readRDS("/Users/polinasuter/Downloads/DBNs/DBNclass/data/datanorm122.rds")
+datacancer<-readRDS("/Users/polinasuter/Downloads/DBNs/DBNclass/data/datacancer122.rds")
 #curbl - blacklist prohibiting intra-edges
-curbl<-readRDS("data/bl_122i.rds")
+curbl<-readRDS("/Users/polinasuter/Downloads/DBNs/DBNclass/data/bl_122i.rds")
 #threshold for consensus model
 p<-0.9
 
 library(BiDAG)
-pars_ns<-scoreparameters("bge",datanorm,
+pars_ns_normal<-scoreparameters("bge",datanorm,
                          dbnpar=list(samestruct=FALSE, slices=4,b=0,datalist=TRUE,stationary=FALSE),
                          DBN=TRUE,edgepmat=NULL)
-sampn_ns <- orderMCMC(pars_ns, startspace=commonspace,chainout = TRUE,
+sampn_ns_normal<-orderMCMC(pars_ns_normal, startspace=commonspace,chainout=TRUE,
                       verbose=TRUE,blacklist=curbl)
-consn_ns<-modelp(sampn_ns,p=0.9,pdag=FALSE)
+consn_ns_normal<-modelp(sampn_ns_normal,p=0.9,pdag=FALSE)
+
+pars_ns_cancer<-scoreparameters("bge",datacancer,
+                         dbnpar=list(samestruct=FALSE, slices=4,b=0,datalist=TRUE,stationary=FALSE),
+                         DBN=TRUE,edgepmat=NULL)
+sampn_ns_cancer <-orderMCMC(pars_ns_cancer, startspace=commonspace,chainout = TRUE,
+                     verbose=TRUE,blacklist=curbl)
+consn_ns_cancer<-modelp(sampn_ns_cancer,p=0.9,pdag=FALSE)
+
+compareDBNs(consn_ns_normal,consn_ns_cancer)
+
 
